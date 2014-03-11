d48e66f47cb9c0a6ab8d88bb5ec97d11f654c94b
Eirik Torske <eirik.torske@gmail.com>
2014-03-11 11:01:48 +0100

    Trying to mend Heroku build fail, Bower removed

--- client/scripts/app.config.js
+++ client/scripts/app.config.js
@@ -1,15 +1,15 @@
 /* global require: false */
 require.config({
     paths: {
-        'jquery': '../bower_components/jquery/dist/jquery',
-        'jquery.bootstrap': '../bower_components/bootstrap/dist/js/bootstrap'
-    },
-    shim: {
-        'jquery.bootstrap': {
-            deps: ['jquery'],
-            exports: 'Bootstrap'
-        }
-    }
+        'jquery': './vendor/jquery.min'//,
+        //'jquery.bootstrap': './vendor/bootstrap.min'
+    }//,
+    //shim: {
+    //    'jquery.bootstrap': {
+    //        deps: ['jquery'],
+    //        exports: 'Bootstrap'
+    //    }
+    //}
 });
 
 // Load and start
