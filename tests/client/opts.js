fc0d710c5a8ecb9ec4393b47ec04489722bdcdac
Eirik Torske <eirik.torske@gmail.com>
2014-11-01 14:44:16 +0100

    Sauce Labs iteration

--- tests/client/opts.js
+++ tests/client/opts.js
@@ -1,5 +1,9 @@
-describe('Options', function() {
-    it('should set timeout value', function() {
+/* global root:false */
+/* jshint -W033, -W117 */
+
+describe('Options', function () {
+    "use strict";
+    it('should set timeout value', function () {
         assert(this.test._timeout === 1500);
     });
 })
